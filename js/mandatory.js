853d9aa56261711d972fd29edef9c76bef8a3854
v26inay <70798059+v26inay@users.noreply.github.com>
2021-07-24 18:28:29 +0530

    Add files via upload

---
+++ js/mandatory.js
@@ -0,0 +1,38 @@
+(function (global) {
+    if(typeof (global) === "undefined") {
+        throw new Error("window is undefined");
+    }
+    var _hash = "!";
+    var noBackPlease = function () {
+        global.location.href += "#";
+        global.setTimeout(function () {
+            global.location.href += "!";
+        }, 50);
+    };
+    global.onhashchange = function () {
+        if (global.location.hash !== _hash) {
+            global.location.hash = _hash;
+        }
+    };
+    global.onload = function () {
+        noBackPlease();
+        document.body.onkeydown = function (e) {
+            var elm = e.target.nodeName.toLowerCase();
+            if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
+                e.preventDefault();
+            }
+            e.stopPropagation();
+        };
+    }
+})(window);
+$(document).keydown(function(event){
+    if(event.keyCode==123){
+        return false;
+    }
+    else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
+             return false;
+    }
+});
+$(document).on("contextmenu",function(e){        
+   e.preventDefault();
+});
