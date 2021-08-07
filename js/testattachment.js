2be527b609010c59702b2af072fb1e6ce1603e64
v26inay <70798059+v26inay@users.noreply.github.com>
2021-08-07 14:36:30 +0530

    Add files via upload

---
+++ js/testattachment.js
@@ -0,0 +1,19 @@
+function ValidateSize(file) {
+	var FileSize = file.files[0].size / 1024 / 1024;
+
+    if (FileSize > 2) {
+        alert('Filesize must not exceed 2 MB.');
+        $(file).val('');
+    } 
+    else {
+        var fileExtension = ['pdf', 'docx', 'xlsx', 'pptx', 'msg' , 'zip', 'rar', '7z'];
+
+        if ($.inArray($(image).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+            alert("Allowed filetypes: PDF, Word, Excel, PowerPoint, OutlookMail, ZIP, RAR and 7z.");
+            $(image).val('');
+        }
+    }
+}
+function vinayonline () {
+    
+}
